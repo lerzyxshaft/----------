4a613cdbe6abda27418be65ac3034c8e345418ef
Maksym <shaftyt12@gmail.com>
2024-11-08 01:58:39 +0300

    081124

--- new.proj.py
+++ new.proj.py
@@ -3,15 +3,19 @@ class Cat:
     age = None 
     isHappy = None
     
+    def set_data(self, name, age, isHappy):
+        self.name = name
+        self.age = age
+        self.isHappy = isHappy 
+
+    def get_data(self):
+        print(self.name, "age :", self.age, "isHappy :", self.isHappy)
+
 cat1 = Cat()
-cat1.name = "Barsik"
-cat1.age = "3"
-Cat.isHappy = True
+cat1.set_data("Barsik", 3, True)
 
 cat2 = Cat()
-cat2.name = "Murka"
-cat2.age = "5"
-cat2.isHappy = False
+cat2.set_data("Any", 5 , False)
 
-print(cat1.name)
-print(cat2.name)
+cat1.get_data()
+cat2.get_data()
